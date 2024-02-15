92e8fe5bc239afd7e536e8bfe306134f75b519dd
Squard31 <jidfai@gmail.com>
2024-02-15 11:58:24 +0100

    TP1 presque finit

---
+++ src/personnage/Village.java
@@ -0,0 +1,16 @@
+package personnage;
+
+public class Village 
+{
+	private String nom;
+	
+	public Village(String nom) 
+	{
+		this.nom = nom;
+	}
+	
+	public String getNom() 
+	{
+		return nom;
+	}
+}
