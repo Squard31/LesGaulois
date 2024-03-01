fb0979ce04564c5e030067aedd025156157e5302
Squard31 <jidfai@gmail.com>
2024-03-01 08:39:40 +0100

    2 premiers méthodes et attributs ajoutés

--- src/personnage/Village.java
+++ src/personnage/Village.java
@@ -3,14 +3,55 @@ package personnage;
 public class Village 
 {
 	private String nom;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
 	
-	public Village(String nom) 
+	// Constructeur de Village
+	public Village(String nom, int nbVillageoisMaximum) 
 	{
 		this.nom = nom;
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
+	// Pour retourner le nom
 	public String getNom() 
 	{
 		return nom;
 	}
+	
+	public void ajouterHabitant(Gaulois gaulois)
+	{
+		if (nbVillageois < nbVillageoisMaximum)
+		{
+			villageois[nbVillageois] = gaulois;
+			nbVillageois = nbVillageois + 1;
+		}
+		else
+		{
+			System.out.println("On a atteint le maximum de perosnne dans le village");
+		}
+	}
+	
+	
+	public Gaulois trouverHabitant(int numeroVillageois)
+	{
+		if (numeroVillageois < nbVillageoisMaximum)
+		{
+			return villageois[numeroVillageois];
+		}
+		else
+		{
+			System.out.println("Village plein !");
+		}
+		
+	}
+	
+	
+	public void main(String[] args)
+	{
+		
+	}
+	
 }
